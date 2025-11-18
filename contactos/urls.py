322326369c0bdfc6e1c4df876cc374a88b6c8295
Maythem11O6 <wolf2.0game@gmail.com>
2025-11-18 14:25:37 -0300

    repo backend unidad 3

--- contactos/urls.py
+++ contactos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views 
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 router = routers.DefaultRouter()
 router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
@@ -13,5 +14,8 @@ urlpatterns = [
     path('<int:contacto_id>/', views.detalle_contacto, name='detalle_contacto'),
     path('<int:contacto_id>/editar/', views.editar_contacto, name='editar_contacto'),
     path('<int:contacto_id>/eliminar/', views.eliminar_contacto, name='eliminar_contacto'),
-    path('test/', views.test_view, name='test_view') 
+    path('test/', views.test_view, name='test_view'),
+    # Rutas JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
